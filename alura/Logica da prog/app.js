3e630b2fd2c532fac7911b8019b691931e0b0b19
kakaszx <kauaaurelio14@gmail.com>
2025-04-28 17:20:06 -0300

    up

---
+++ alura/Logica da prog/app.js
@@ -0,0 +1,21 @@
+alert("Boas vindas ao jogo do número secreto !");
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+//enquanto o chute nao for o número secreto
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número entre 0 e 10");
+  // se chute for igual ao número secreto
+  if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);  
+  } else {
+    if (numeroSecreto > chute) {
+      alert(`O número secreto é maior que ${chute}`);
+    } else {
+      alert(`O número secreto é menor que ${chute}`);
+    }
+    tentativas++; 
+  }
+}
