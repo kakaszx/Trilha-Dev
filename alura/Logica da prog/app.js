0f26e4cb3d897a0b9aac859c703748a951f35b64
kakaszx <kauaaurelio14@gmail.com>
2025-04-28 18:22:12 -0300

    up

--- alura/Logica da prog/app.js
+++ alura/Logica da prog/app.js
@@ -19,3 +19,4 @@ while (chute != numeroSecreto) {
     tentativas++; 
   }
 }
+//a
---
+++ alura/Logica da prog/desafios/logica-js-projeto_inicial/app.js
@@ -0,0 +1,68 @@
+//desafio01
+// diaDaSemana = prompt('Qual é o dia da semana?');
+// if (diaDaSemana == 'Sábado') {
+//     alert('Bom fim de semana!');
+// } else if (diaDaSemana == 'Domingo') {
+//     alert('Bom fim de semana!');
+// } else {
+//     alert('Boa semana!');
+// }
+
+// desafio02
+// numero = prompt('Digite um positivo ou negativo');
+// if (numero > 0) {
+//     alert('Número positivo!');
+// } else {
+//     alert('Número negativo!');
+// }
+
+// desafio03
+// pontuacao = prompt('Quantos monstros você matou?');
+// if (pontuacao <= 10) {
+//     alert('Você é um fraco!');
+// }else if (pontuacao <= 20) {
+//     alert('Você é mediano!');
+// }else {
+//     alert('Parabens! você é um fortinho');
+// }
+
+// desafio04
+// let saldo = 200; // Exemplo de saldo
+// alert(`Seu saldo é de R$${saldo}.`);
+
+//desafio05
+// let nome = prompt('Qual o seu nome?');
+// alert(`Seja bem-vindo ${nome}`);
+
+//deafio06
+//  let contador = 1;
+//  while (contador <= 10) {
+//      console.log(contador);
+//      contador++;
+//  }
+
+//deafio07
+// let contador = 10;
+// while (contador >= 0) {
+//   console.log(contador);
+//   contador--;
+// }
+
+//desafio08
+//  let contador = prompt('Digite um número pra iniciar a contagem regressiva');
+
+// while (contador >= 0) {
+//   console.log(contador);
+//   contador--;
+// }
+
+//desafio09
+// let numero = prompt("Digite um número para a contagem progregressiva:");
+// let contador = 0;
+
+// while (contador <= numero) {
+//     console.log(contador);
+//     contador++
+// }
+
+//desafio10
