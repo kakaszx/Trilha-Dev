55d86419f7b2b38e9e3056b8221f73524f828126
kakaszx <kauaaurelio14@gmail.com>
2025-04-29 15:19:48 -0300

    up

--- alura/Logica da prog/app.js
+++ alura/Logica da prog/app.js
@@ -1,21 +1,37 @@
 alert("Boas vindas ao jogo do número secreto !");
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto o chute nao for o número secreto
 while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 0 e 10");
+  chute = prompt("Escolha um número entre 0 e 100");
   // se chute for igual ao número secreto
   if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);  
+    break;
   } else {
     if (numeroSecreto > chute) {
       alert(`O número secreto é maior que ${chute}`);
     } else {
       alert(`O número secreto é menor que ${chute}`);
     }
-    tentativas++; 
+    tentativas++;
   }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}. `
+);
+
+// if (tentativas > 1) {
+  
+//   alert(
+//     `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`
+//   );
+// } else {
+//   alert(
+//     `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`
+//   );
+// }
