df5835f995ba9bb1b53e251f32dc817221d40d55
kakaszx <kauaaurelio14@gmail.com>
2025-05-03 16:01:10 -0300

    up

--- alura/Lógica de programação/desafios/logica-js-projeto_inicial/app.js
+++ alura/Lógica de programação/desafios/logica-js-projeto_inicial/app.js
@@ -297,3 +297,47 @@
 // let temperaturaCelsius = converterTemperatura(temperaturaFahrenheit, "F");
 // console.log(temperaturaCelsius)
 
+// desafio 35 Manipulando Arrays
+// Declarando uma variável do tipo array
+// let minhaLista = [];
+
+// // Adicionando elementos com push
+// minhaLista.push(1, 2, 3);
+// console.log("Adicionando elementos:", minhaLista);
+
+// // Criando uma nova variável
+// let outrosNumeros = [4, 5, 6];
+
+// // Concatenando arrays
+// let novaLista = minhaLista.concat(outrosNumeros);
+// console.log("Juntando Arrays:", novaLista);
+
+//desafio 36 Removendo elementos com pop
+// Removendo o último elemento
+// novaLista.pop();
+// console.log("Desafio 4:", novaLista);
+
+//desafio 37 Embaralhando elementos em um array Incremente com as informações a partir do Desafio 2
+
+// function embaralharArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array;
+//   }
+  
+//   // Embaralhando novaLista
+//   novaLista = embaralharArray(novaLista);
+//   console.log("Embaralhando a Lista:", novaLista);
+
+//desafio 38 Removendo duplicatas
+
+// Função para remover duplicatas de um array
+// function removerDuplicatas(array) {
+//     return [...new Set(array)];
+//   }
+  
+//   // Testando a função com novaLista
+//   let novaListaSemDuplicatas = removerDuplicatas(novaLista);
+//   console.log("Remover duplicatas:", novaListaSemDuplicatas);
