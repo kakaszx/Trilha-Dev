9c627d82e0769b3aa9d6cc93bea512e943eb6dd6
kakaszx <kauaaurelio14@gmail.com>
2025-05-05 12:30:31 -0300

    up

---
+++ alura/Javascript/6-listas.js
@@ -0,0 +1,16 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const RioDeJaneiro = `Rio de Janeiro`;
+
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+listaDeDestinos.push(`Curitiba`) //adicionando um item na lista
+console.log("Destinos possiveis");
+// console.log(salvador, saoPaulo, RioDeJaneiro);
+console.log(listaDeDestinos);
