8d2f2fa45b94af21048d402d213d3c7595330822
kakaszx <kauaaurelio14@gmail.com>
2025-05-07 16:00:07 -0300

    up

--- alura/Javascript/projeto bytebank/ContaPoupanca.js
+++ alura/Javascript/projeto bytebank/ContaPoupanca.js
@@ -4,7 +4,6 @@ export class ContaPoupanca extends Conta {
   constructor(saldoInicial, cliente, agencia) {
     super(saldoInicial, cliente, agencia);
   }
-
   sacar(valor) {
     const taxa = 1.02;
     return this._sacar(valor, taxa);
