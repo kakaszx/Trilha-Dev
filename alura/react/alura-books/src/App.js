46bc1ba6a77ff7f10ebd0f907d28e6f1d71c97ff
kakaszx <kauaaurelio14@gmail.com>
2025-05-16 23:04:32 -0300

    up

---
+++ alura/react/alura-books/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo">
+          <img src={logo} alt="logo"></img>
+          <p>
+            <strong>Alura </strong>Books
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
