cf2a2ad25108722adc1debfdc043126951d50125
kakaszx <kauaaurelio14@gmail.com>
2025-05-20 20:21:01 -0300

    up

--- alura/react/alura-books/src/componentes/Logo/index.js
+++ alura/react/alura-books/src/componentes/Logo/index.js
@@ -1,9 +1,10 @@
+import "./estilo.css";
 import logo from "../../imagens/logo.svg";
 
 function Logo() {
   return (
     <div className="logo">
-      <img src={logo} alt="logo"></img>
+      <img className="logo-img" src={logo} alt="logo"></img>
       <p>
         <strong>Alura </strong>Books
       </p>
