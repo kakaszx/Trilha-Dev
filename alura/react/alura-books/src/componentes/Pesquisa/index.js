5c841b7694b586aa0f1be7b6b5aabc84abf2dff9
kakaszx <kauaaurelio14@gmail.com>
2025-05-20 23:46:39 -0300

    up

--- alura/react/alura-books/src/componentes/Pesquisa/index.js
+++ alura/react/alura-books/src/componentes/Pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
+import { useState } from "react";
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -20,13 +22,46 @@ const Subtitulo = styled.h3`
   font-weight: 500;
   margin-bottom: 40px;
 `;
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
 
 function Pesquisa() {
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
   return (
     <PesquisaContainer>
       <Titulo>Já sabe por onde começar?</Titulo>
       <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-      <Input placeholder="Escreva sua próxima leitura" />
+      <Input
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resultadoPesquisa = livros.filter((livro) =>
+            livro.nome.includes(textoDigitado)
+          );
+          setLivrosPesquisados(resultadoPesquisa);
+        }}
+      />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <p>{livro.nome} </p>
+          <img src={livro.src} />
+        </Resultado>
+      ))}
     </PesquisaContainer>
   );
 }
